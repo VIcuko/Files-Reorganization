06989a22b966f57d2221bf50bb014534d607ae3e
Vicuko <victorc.fernandez@gmail.com>
2017-06-11 00:44:39 +0200

    Retrieve current working directory

--- rename_files.py
+++ rename_files.py
@@ -3,5 +3,13 @@ import os
 def rename_files():
 	file_list = os.listdir("./files")
 	print(file_list)
+	
+	saved_path = os.getcwd()
+	print ("Current working directory is: " + saved_path)
 
+        
+
+	for file_name in file_list:
+                os.rename("./files/" + file_name, file_name.translate(None,"0123456789"))
+		
 rename_files()
