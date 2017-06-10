6b47a6a148b69c3a47e72d252b04788d61b46f1a
Vicuko <victorc.fernandez@gmail.com>
2017-06-11 00:48:00 +0200

    Modify file names to delete all numbers

--- rename_files.py
+++ rename_files.py
@@ -1,15 +1,17 @@
 import os
 
 def rename_files():
-	file_list = os.listdir("./files")
+	file_list = os.listdir("/Users/iTurbo/Google Drive/3. Developer/4. Python/Udacity/files_reorganization/files")
 	print(file_list)
 	
 	saved_path = os.getcwd()
 	print ("Current working directory is: " + saved_path)
 
-        
+        os.chdir("/Users/iTurbo/Google Drive/3. Developer/4. Python/Udacity/files_reorganization/files")
 
 	for file_name in file_list:
-                os.rename("./files/" + file_name, file_name.translate(None,"0123456789"))
+                os.rename(file_name, file_name.translate(None,"0123456789"))
 		
+	os.chdir(saved_path)
+	
 rename_files()
